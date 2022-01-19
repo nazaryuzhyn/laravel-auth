52879972f53f46f2f01b33109f4e600219bbf43b
nazaryuzhyn <nazaryuzhyn@gmail.com>
2022-01-19 18:47:48 +0200

    init

---
+++ src/Providers/LaravelAuthServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace LaravelAuth\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class LaravelAuthServiceProvider extends ServiceProvider
+{
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->loadRoutesFrom(__DIR__ . 'Routes/api.php');
+    }
+}
