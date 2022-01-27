061419aa213120e40c0ff903b487a14391b7f4e4
nazaryuzhyn <nazaryuzhyn@gmail.com>
2022-01-27 17:17:43 +0200

    added tests

---
+++ tests/SetUpDatabase.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace LaravelAuth\Tests;
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Foundation\Application;
+
+trait SetUpDatabase
+{
+    /**
+     * @param Application $app
+     */
+    protected function setUpDatabase($app)
+    {
+        $app['db']->connection()
+            ->getSchemaBuilder()
+            ->create('users', static function (Blueprint $table) {
+                $table->id();
+                $table->string('name');
+                $table->string('email')->unique();
+                $table->timestamp('email_verified_at')->nullable();
+                $table->string('password');
+                $table->rememberToken();
+                $table->timestamps();
+            });
+
+        $app['db']->connection()
+            ->getSchemaBuilder()
+            ->create('password_resets', static function (Blueprint $table) {
+                $table->string('email')->index();
+                $table->string('token');
+                $table->timestamp('created_at')->nullable();
+            });
+
+        $app['db']->connection()
+            ->getSchemaBuilder()
+            ->create('personal_access_tokens', static function (Blueprint $table) {
+                $table->id();
+                $table->morphs('tokenable');
+                $table->string('name');
+                $table->string('token', 64)->unique();
+                $table->text('abilities')->nullable();
+                $table->timestamp('last_used_at')->nullable();
+                $table->timestamps();
+            });
+    }
+}
